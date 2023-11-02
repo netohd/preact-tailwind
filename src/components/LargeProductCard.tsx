257b304c45b7a48721effd8fb78b442adc501f83
Antonio Almeida <toninetocontato@gmail.com>
2023-11-02 18:54:48 -0300

    large product card

--- src/components/LargeProductCard.tsx
+++ src/components/LargeProductCard.tsx
@@ -1,22 +1,18 @@
 const LargeProductCard = ({ imageUrl, name, discount }) => {
   return (
-    <div class="m-3 bg-zinc-900 rounded-lg w-96 flex">
-      
-      <div class="p-4 flex flex-col bg-zinc-800 w-full rounded-lg">
-        <p class="pb-3 pt-1 text-lg">{name}</p>
-        <s class="text-sm text-zinc-500">{discount}</s>
-        <p class="font-bold text-lg">{discount}</p>
-        <div class="flex flex-row items-center">
-          <p class="text-yellow-500 text-sm pr-2">ganhe 120</p>
-          <img class="h-4" src="https://gcdnb.pbrd.co/images/Kn8qJxENZLJv.png" alt="moedas" />
-        </div>
+    <div class="m-3 bg-zinc-900 rounded-lg w-[400px] h-[200px] flex justify-between">
+
+      <div class="p-4 flex flex-col bg-zinc-200 w-full rounded-l-lg">
+        <p class="font-bold text-lg pt-4">ATÉ {discount}% OFF</p>
+        <p class="leading-8 pb-3 pt-1 text-3xl text-zinc-900 font-bold">{name}</p>
+        <button class="text-white text-sm bg-zinc-700 mt-4 py-1 w-20 rounded-md">Veja mais</button>
       </div>
 
-      <div class="h-auto rounded-lg">
+      <div class="flex rounded-lg w-full">
         <img
           alt="produto"
           src={imageUrl}
-          class="object-fill rounded-r-lg"
+          class="object-cover rounded-r-lg"
         />
       </div>
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
